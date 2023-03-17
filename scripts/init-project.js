24318c96381b8f120c0857b3060089255115f513
Lars Vonk <larserikvonk@ziggo.nl>
2023-03-17 11:33:13 +0100

    feat: moved docker-compose to template

--- scripts/init-project.js
+++ scripts/init-project.js
@@ -1,7 +1,11 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const { execSync } = require('child_process');
-const path = require('path');
+import inquirer from 'inquirer';
+import fs from 'fs';
+import { execSync } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const questions = [
 	{
@@ -11,29 +15,36 @@ const questions = [
 	},
 ];
 
+function executeCommand(command) {
+	execSync(command, { stdio: 'inherit' });
+}
+
 inquirer.prompt(questions).then((answers) => {
 	answers.name = answers.name.toLowerCase().replace(/ /g, '-');
 
 	const { name: projectName } = answers;
 
-	const dockerCompose = fs.readFileSync('./docker-compose.yml', 'utf8');
+	const dockerCompose = fs.readFileSync(path.join(__dirname, 'templates', 'docker-compose.yml'), 'utf8');
 
 	const newDockerCompose = dockerCompose.replaceAll('${PROJECT_NAME}', projectName);
 
-	fs.writeFileSync('./docker-compose.yml', newDockerCompose, 'utf8');
+	fs.writeFileSync(path.join(__dirname, '..', 'docker-compose.yml'), newDockerCompose, 'utf8');
 
-	execSync(`
-        cd ${path.join(__dirname, 'packages', 'client')} && 
-        vue create .
+	executeCommand(`
+        cd ${path.join(__dirname, '..', 'packages', 'client')} && 
+        npx @vue/cli create .
     `);
-	execSync(`
-        cd ${path.join(__dirname, 'packages', 'server')} && 
+	executeCommand(`
+        cd ${path.join(__dirname, '..', 'packages', 'server')} && 
         git clone https://github.com/thebetar/express-template && 
         rm -rf .git
     `);
-	execSync(`
-        docker run --name ${projectName}-db-container -v ${path.join(__dirname, 'packages', 'db', 'data')} -p 27017:27017 -d mongo
-    `);
+	// executeCommand(`
+	//     docker run --name ${projectName}-db-container -v ${path.join(__dirname, '..', 'packages', 'db', 'data')} -p 27017:27017 -d mongo
+	// `);
+	executeCommand(`
+		npm install
+	`);
 
 	console.log('Project name set to: ' + answers.name);
 });
