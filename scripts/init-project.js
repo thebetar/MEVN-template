336acb10de6f54b16b50ba1834c728df96c2edc8
Lars Vonk <larserikvonk@ziggo.nl>
2023-03-17 15:51:11 +0100

    feat: added dockerfile for client

--- scripts/init-project.js
+++ scripts/init-project.js
@@ -19,29 +19,48 @@ function executeCommand(command) {
 	execSync(command, { stdio: 'inherit' });
 }
 
+function createClient(projectName) {
+	executeCommand(`
+        cd ${path.join(__dirname, '..', 'packages', 'client')} && 
+        npm create vue@latest . --no-git --skipGetStarted --bare
+    `);
+
+	executeCommand(`
+		cp ${path.join(__dirname, 'templates', 'client', 'Dockerfile')} ${path.join(__dirname, '..', 'packages', 'client')}
+	`);
+
+	const clientNginxConfig = fs.readFileSync(path.join(__dirname, 'templates', 'client', 'nginx.conf'), 'utf8');
+	const newClientNginxConfig = clientNginxConfig.replaceAll('${PROJECT_NAME}', projectName);
+	fs.writeFileSync(path.join(__dirname, '..', 'packages', 'client', 'nginx.conf'), newClientNginxConfig, 'utf8');
+}
+
+function createServer() {
+	executeCommand(`
+	cd ${path.join(__dirname, '..', 'packages', 'server')} && 
+	git clone https://github.com/thebetar/express-template . && 
+	rm -rf .git
+`);
+}
+
+function createDb(projectName) {
+	executeCommand(`
+	    docker run --name ${projectName}-db-container -v ${path.join(__dirname, '..', 'packages', 'db', 'data')} -p 27017:27017 -d mongo
+	`);
+}
+
 inquirer.prompt(questions).then((answers) => {
 	answers.name = answers.name.toLowerCase().replace(/ /g, '-');
 
 	const { name: projectName } = answers;
 
 	const dockerCompose = fs.readFileSync(path.join(__dirname, 'templates', 'docker-compose.yml'), 'utf8');
-
 	const newDockerCompose = dockerCompose.replaceAll('${PROJECT_NAME}', projectName);
-
 	fs.writeFileSync(path.join(__dirname, '..', 'docker-compose.yml'), newDockerCompose, 'utf8');
 
-	executeCommand(`
-        cd ${path.join(__dirname, '..', 'packages', 'client')} && 
-        npm create vue@latest . --no-git --skipGetStarted --bare
-    `);
-	executeCommand(`
-        cd ${path.join(__dirname, '..', 'packages', 'server')} && 
-        git clone https://github.com/thebetar/express-template . && 
-        rm -rf .git
-    `);
-	executeCommand(`
-	    docker run --name ${projectName}-db-container -v ${path.join(__dirname, '..', 'packages', 'db', 'data')} -p 27017:27017 -d mongo
-	`);
+	createClient(projectName);
+	createServer();
+	createDb(projectName);
+
 	executeCommand(`
 		npm install
 	`);
